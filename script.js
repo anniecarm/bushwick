9a213214e161bae53ea0b4dada401eb34647cf4d
Annie Carmichael <42788203+anniecarm@users.noreply.github.com>
2019-03-14 09:04:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, );
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.slider').slider();
+  });
+
+   document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, );
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+        
